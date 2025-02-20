70ef3121914e9579877463713fa65ad83d5e83b9
a_bakos <bakosbence1@icloud.com>
2025-02-20 10:10:26 +0100

    működik az e-mail cím is

--- backend/security_verification_code.php
+++ backend/security_verification_code.php
@@ -13,46 +13,64 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Ellenőrizzük, hogy jelszó vagy e-mail cím változtatásról van-e szó
     if (isset($_POST['new_email'])) {
-        $newEmail = $_POST['new_email'];
-        $_SESSION['new_email'] = $newEmail;
-        $subject = 'Megerősítő kód az e-mail cím változtatásához';
-        $body = "Kedves Felhasználó,<br><br>Az Ön megerősítő kódja az e-mail cím változtatásához: <b>$verificationCode</b><br><br>Üdvözlettel,<br>watch.it";
-
-        // Kód mentése az adatbázisba
         $userId = $_SESSION['user_id'];
-        $stmt = $conn->prepare("UPDATE account SET verification_code = ? WHERE id = ?");
-        $stmt->bind_param("si", $verificationCode, $userId);
+        $oldEmail = $_POST['oldEmail'];
+        $newEmail = $_POST['new_email'];
+
+        // Ellenőrizzük, hogy a megadott régi e-mail cím megegyezik-e az adatbázisban tárolt e-mail címmel
+        $sql = "SELECT email FROM account WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $userId);
         $stmt->execute();
-        $stmt->close();
+        $stmt->store_result();
+        $stmt->bind_result($currentEmail);
+        $stmt->fetch();
 
-        // E-mail küldése a felhasználónak a kóddal
-        $mail = new PHPMailer(true);
+        if ($stmt->num_rows > 0 && $currentEmail === $oldEmail) {
+            $stmt->close();
 
-        try {
-            // SMTP beállítások
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com'; // SMTP szerver címe
-            $mail->SMTPAuth = true;
-            $mail->Username = 'noreply.watch.it@gmail.com'; // SMTP felhasználónév
-            $mail->Password = 'ynzd bgae pokl ukuw'; // Alkalmazásjelszó
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
+            $_SESSION['new_email'] = $newEmail;
+            $subject = 'Megerősítő kód az e-mail cím változtatásához';
+            $body = "Kedves Felhasználó,<br><br>Az Ön megerősítő kódja az e-mail cím változtatásához: <b>$verificationCode</b><br><br>Üdvözlettel,<br>watch.it";
 
-            // E-mail beállítások
-            $mail->CharSet = 'UTF-8'; // Karakterkódolás beállítása
-            $mail->setFrom('noreply.watch.it@gmail.com', 'watch.it');
-            $mail->addAddress($newEmail);
-            $mail->isHTML(true);
-            $mail->Subject = $subject;
-            $mail->Body = $body;
+            // Kód mentése az adatbázisba
+            $stmt = $conn->prepare("UPDATE account SET verification_code = ? WHERE id = ?");
+            $stmt->bind_param("si", $verificationCode, $userId);
+            $stmt->execute();
+            $stmt->close();
 
-            $mail->send();
-            
-            $response['status'] = 'success';
-            $response['message'] = 'A megerősítő kódot elküldtük az új e-mail címére.';
-        } catch (Exception $e) {
+            // E-mail küldése a felhasználónak a kóddal
+            $mail = new PHPMailer(true);
+
+            try {
+                // SMTP beállítások
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com'; // SMTP szerver címe
+                $mail->SMTPAuth = true;
+                $mail->Username = 'noreply.watch.it@gmail.com'; // SMTP felhasználónév
+                $mail->Password = 'ynzd bgae pokl ukuw'; // Alkalmazásjelszó
+                $mail->SMTPSecure = 'tls';
+                $mail->Port = 587;
+
+                // E-mail beállítások
+                $mail->CharSet = 'UTF-8'; // Karakterkódolás beállítása
+                $mail->setFrom('noreply.watch.it@gmail.com', 'watch.it');
+                $mail->addAddress($newEmail);
+                $mail->isHTML(true);
+                $mail->Subject = $subject;
+                $mail->Body = $body;
+
+                $mail->send();
+                
+                $response['status'] = 'success';
+                $response['message'] = 'A megerősítő kódot elküldtük az új e-mail címére.';
+            } catch (Exception $e) {
+                $response['status'] = 'error';
+                $response['message'] = 'Hiba történt az e-mail küldése során: ' . $mail->ErrorInfo;
+            }
+        } else {
             $response['status'] = 'error';
-            $response['message'] = 'Hiba történt az e-mail küldése során: ' . $mail->ErrorInfo;
+            $response['message'] = 'Helytelen régi e-mail cím';
         }
     } elseif (isset($_POST['oldPassword'])) {
         $userId = $_SESSION['user_id'];
@@ -108,62 +126,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             $response['status'] = 'error';
             $response['message'] = 'Helytelen régi jelszó';
-        }
-    } elseif (isset($_POST['oldEmail'])) {
-        $userId = $_SESSION['user_id'];
-        $oldEmail = $_POST['oldEmail'];
-        $newEmail = $_POST['new_email'];
-
-        $sql = "SELECT email FROM account WHERE id = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("i", $userId);
-        $stmt->execute();
-        $stmt->store_result();
-        $stmt->bind_result($currentEmail);
-        $stmt->fetch();
-
-        if ($stmt->num_rows > 0 && $currentEmail === $oldEmail) {
-            $subject = 'Megerősítő kód az e-mail cím változtatásához';
-            $body = "Kedves Felhasználó,<br><br>Az Ön megerősítő kódja az e-mail cím változtatásához: <b>$verificationCode</b><br><br>Üdvözlettel,<br>watch.it";
-
-            // Kód mentése az adatbázisba
-            $stmt = $conn->prepare("UPDATE account SET verification_code = ? WHERE id = ?");
-            $stmt->bind_param("si", $verificationCode, $userId);
-            $stmt->execute();
-            $stmt->close();
-
-            // E-mail küldése a felhasználónak a kóddal
-            $mail = new PHPMailer(true);
-
-            try {
-                // SMTP beállítások
-                $mail->isSMTP();
-                $mail->Host = 'smtp.gmail.com'; // SMTP szerver címe
-                $mail->SMTPAuth = true;
-                $mail->Username = 'noreply.watch.it@gmail.com'; // SMTP felhasználónév
-                $mail->Password = 'ynzd bgae pokl ukuw'; // Alkalmazásjelszó
-                $mail->SMTPSecure = 'tls';
-                $mail->Port = 587;
-
-                // E-mail beállítások
-                $mail->CharSet = 'UTF-8'; // Karakterkódolás beállítása
-                $mail->setFrom('noreply.watch.it@gmail.com', 'watch.it');
-                $mail->addAddress($newEmail);
-                $mail->isHTML(true);
-                $mail->Subject = $subject;
-                $mail->Body = $body;
-
-                $mail->send();
-                
-                $response['status'] = 'success';
-                $response['message'] = 'A megerősítő kódot elküldtük az új e-mail címére.';
-            } catch (Exception $e) {
-                $response['status'] = 'error';
-                $response['message'] = 'Hiba történt az e-mail küldése során: ' . $mail->ErrorInfo;
-            }
-        } else {
-            $response['status'] = 'error';
-            $response['message'] = 'Helytelen régi e-mail cím';
         }
     }
 }
